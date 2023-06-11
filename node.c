a7a00c78a77a03793b464a5235acd19283e3a9e3
Half_dead <84064967+bsws5132@users.noreply.github.com>
2023-06-11 19:14:14 +0900

    Add files via upload

---
+++ node.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "node.h"
+
+void displayInorder(treeNode* root)
+{
+	if (root)
+	{
+		displayInorder(root->left);
+		printf("%c_", root->key);
+		displayInorder(root->right);
+	}
+}
